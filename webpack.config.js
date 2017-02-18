1b6b9d0894625b22df8bea24ff95f341927ecd27
Tatiana_Bessonova <tatiana_bessonova@epam.com>
2017-02-18 14:21:12 +0300

    separation of production and development configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
+
 const PATHS = {
     source: path.join(__dirname, 'src'),
     build: path.join(__dirname, 'build')
@@ -9,15 +13,15 @@ module.exports = function(env) {
         return common;
     }
     if (env == 'development') {
-        return Object.assign(
-            {},
+        return merge([
             common,
-            developmentConfig
-        );
+            devserver()
+        ]);
     }
 };
 
-const common = {
+const common = merge([
+    {
     entry: {
         'index': PATHS.source + '/pages/index/index.js',
         'blog': PATHS.source + '/pages/blog/blog.js'
@@ -26,33 +30,18 @@ const common = {
         path: PATHS.build,
         filename: '[name].js'
     },
- plugins: [
-    new HtmlWebpackPlugin({
-        filename: 'index.html',
-        chunks: ['index'],
-        template: PATHS.source + '/pages/index/index.pug'
-    }),
-    new HtmlWebpackPlugin({
-        filename: 'blog.html',
-        chunks: ['blog'],
-        template: PATHS.source + '/pages/blog/blog.pug'
-    }),
- ],
-    module: {
-      rules: [
-        {
-          test: /\.pug$/,
-          loader: 'pug-loader',
-          options: {
-            pretty: true,
-          }
-        }
-      ]
-    }
-};
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            chunks: ['index'],
+            template: PATHS.source + '/pages/index/index.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'blog.html',
+            chunks: ['blog'],
+            template: PATHS.source + '/pages/blog/blog.pug'
+        }),
+    ]},
+    pug()
+]);
 
-const developmentConfig = {
-    devServer: {
-        stats: 'errors-only'
-    }
-};
