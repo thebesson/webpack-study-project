32fe529a8ae1b646d21979adf52339cfc9c922c9
Tatiana_Bessonova <tatiana_bessonova@epam.com>
2017-02-18 12:41:13 +0300

    add pug

---
+++ src/pages/index/index.js
@@ -0,0 +1,4 @@
+import createMenu from './components/menu/menu';
+
+var menu = createMenu(['Главная', 'Блог'],'menu');
+document.body.appendChild(menu);
--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,35 @@ const PATHS = {
     build: path.join(__dirname, 'build')
 };
 module.exports = {
-    entry: PATHS.source + '/index.js',
+    entry: {
+        'index': PATHS.source + '/pages/index/index.js',
+        'blog': PATHS.source + '/pages/blog/blog.js'
+    },
     output: {
         path: PATHS.build,
         filename: '[name].js'
     },
  plugins: [
     new HtmlWebpackPlugin({
-        title: 'Webpack app'
-    })
- ]
+        filename: 'index.html',
+        chunks: ['index'],
+        template: PATHS.source + '/pages/index/index.pug'
+    }),
+    new HtmlWebpackPlugin({
+        filename: 'blog.html',
+        chunks: ['blog'],
+        template: PATHS.source + '/pages/blog/blog.pug'
+    }),
+ ],
+    module: {
+      rules: [
+        {
+          test: /\.pug$/,
+          loader: 'pug-loader',
+          options: {
+            pretty: true,
+          },
+        },
+      ],
+    },
 };
